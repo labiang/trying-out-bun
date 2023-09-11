b28351193eddba9608fbaa78087648641fa74161
Labian Gashi <labiangashi@gmail.com>
2023-09-11 20:57:06 +0200

    Bun server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(request) {
+    return new Response("1 + 1 equals 2");
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
